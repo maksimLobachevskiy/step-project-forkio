a2b5078c6dad662088d454d5e330d2863da0c40a
Vadym Babik <vadym.babik@gmail.com>
2021-01-18 04:06:28 +0200

    top_menu_320px

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
 "use strict";
+document.querySelector(".top-menu__burger").onclick = () => {
+  document.querySelector(".top-menu__burger").classList.toggle("active");
+  document.querySelector(".top-menu__menu").classList.toggle("show");
+};
